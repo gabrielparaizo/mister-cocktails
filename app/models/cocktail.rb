e44f08db14dbaad59da9a0f99c64574a2c5be7d3
gabrielloppes <gabriellopees@hotmail.com>
2020-05-30 17:30:52 -0300

    Mister Cocktail image upload feature

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,6 @@
 class Cocktail < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
   has_many :doses, dependent: :destroy
-  has_many :reviews
   has_many :ingredients, through: :doses
+  has_one_attached :photo
+  validates :name, presence: true, uniqueness: true
 end
